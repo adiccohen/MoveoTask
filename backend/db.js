de9ad3164eca4a7ff804f99814ed7c7576a4e2fe
adiccohen <129376098+adiccohen@users.noreply.github.com>
2024-11-22 22:18:32 +0200

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,12 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "postgres",           // Replace with your PostgreSQL username
-  host: "localhost",          // Replace with cloud host if using a service
-  database: "MoveoTaskDB", // Replace with your database name
-  password: "Mta159753!",   // Replace with your password
-  port: 5432,                  // Default PostgreSQL port
+  host: "autorack.proxy.rlwy.net",          // Replace with cloud host if using a service
+  database: "railway", // Replace with your database name
+  password: "DEIxBjZAAWMqQySAWyZKSaSbudoShNuY",   // Replace with your password
+  port: 38532,                  // Default PostgreSQL port
 });
 
 module.exports = pool;
+
+
